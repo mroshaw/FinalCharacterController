a7672dad6b876c6333b10f1b436861c3272bada5
Oli Ollerenshaw <oli@mroshaw.co.uk>
2024-07-25 14:20:41 +0100

    Moved TPS Camera to it's own component

---
+++ Assets/GinjaGaming/FinalCharacterController/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,44 @@
+
+using GinjaGaming.FinalCharacterController.Input;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace DaftAppleGames.Darskerry.Core.DagCharacterController
+{
+    [DefaultExecutionOrder(-1)]
+    public class ThirdPersonCameraController : MonoBehaviour
+    {
+        #region Class Variables
+        [Header("Components")]
+        [SerializeField] private CinemachineCamera virtualCamera;
+        [Header("Camera Settings")]
+        [SerializeField] private float cameraZoomSpeed = 0.1f;
+        [SerializeField] private float cameraMinZoom = 1f;
+        [SerializeField] private float cameraMaxZoom = 5f;
+
+        private ThirdPersonInput _thirdPersonInput;
+        private CinemachineThirdPersonFollow _thirdPersonFollow;
+        #endregion
+
+        #region Startup
+        private void Awake()
+        {
+            _thirdPersonInput = GetComponent<ThirdPersonInput>();
+            _thirdPersonFollow = virtualCamera.GetComponent<CinemachineThirdPersonFollow>();
+        }
+        #endregion
+
+        #region Update Logic
+        private void Update()
+        {
+            UpdateCameraZoom();
+        }
+
+        private void UpdateCameraZoom()
+        {
+            Vector2 scrollInput = _thirdPersonInput.ScrollInput * cameraZoomSpeed;
+            _thirdPersonFollow.CameraDistance = Mathf.Clamp(_thirdPersonFollow.CameraDistance + scrollInput.y, cameraMinZoom, cameraMaxZoom);
+        }
+        #endregion
+    }
+}
