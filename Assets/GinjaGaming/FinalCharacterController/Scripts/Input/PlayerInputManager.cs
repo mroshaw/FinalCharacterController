c0e53a43aa35f06bd67ca3fbef5ae60a8decd32e
Oli Ollerenshaw <oli@mroshaw.co.uk>
2024-07-25 14:21:39 +0100

    Refactored into specific namespace for input

--- Assets/GinjaGaming/FinalCharacterController/Scripts/Input/PlayerInputManager.cs
+++ Assets/GinjaGaming/FinalCharacterController/Scripts/Input/PlayerInputManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace GinjaGaming.FinalCharacterController
+namespace GinjaGaming.FinalCharacterController.Input
 {
     [DefaultExecutionOrder(-3)]
     public class PlayerInputManager : MonoBehaviour
